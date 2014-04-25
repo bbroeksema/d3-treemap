62d71442aebbb0d19579f9237449d2ad93cdaf24
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-04-25 23:05:19 +0200

    Add a method to calculate itemrects for items in a chunk.
    This is incomplete as it only works for chunks that are phrased
    TOP_LEFT_TO_RIGHT.

--- src/chunk.js
+++ src/chunk.js
@@ -58,6 +58,78 @@
 
     chunk.items = [];
 
+    function calculateItemRect(item, rect) {
+      var x, y, w, h;
+
+      switch(phrase) {
+        case d3.layout.phrase.TOP_LEFT_TO_RIGHT:
+          x = rect.x();
+          y = rect.y();
+          w = chunk.rect().width() * item / chunk.sum();
+          h = chunk.rect().height();
+          return d3_treemap_rect(x, y, w, h);
+        case d3.layout.phrase.LEFT_TOP_TO_BOTTOM:
+        case d3.layout.phrase.LEFT_BOTTOM_TO_TOP:
+        case d3.layout.phrase.LEFT_LEFT_TO_RIGHT:
+        case d3.layout.phrase.LEFT_RIGHT_TO_LEFT:
+        case d3.layout.phrase.RIGHT_BOTTOM_TO_TOP:
+        case d3.layout.phrase.RIGHT_TOP_TO_BOTTOM:
+        case d3.layout.phrase.RIGHT_RIGHT_TO_LEFT:
+        case d3.layout.phrase.RIGHT_LEFT_TO_RIGHT:
+        case d3.layout.phrase.BOTTOM_LEFT_TO_RIGHT:
+        case d3.layout.phrase.BOTTOM_RIGHT_TO_LEFT:
+        case d3.layout.phrase.BOTTOM_BOTTOM_TO_TOP:
+        case d3.layout.phrase.BOTTOM_TOP_TO_BOTTOM:
+        case d3.layout.phrase.TOP_RIGHT_TO_LEFT:
+        case d3.layout.phrase.TOP_TOP_TO_BOTTOM:
+        case d3.layout.phrase.TOP_BOTTOM_TO_TOP:
+          return d3_treemap_rect(0, 0, 0, 0);
+        default:
+          throw("Invalid phrase");
+      }
+    }
+
+    function reduce(chunkRect, itemRect) {
+      switch(phrase) {
+        case d3.layout.phrase.TOP_LEFT_TO_RIGHT:
+          return chunkRect
+            .x(chunkRect.x() + itemRect.width())
+            .width(chunkRect.width() - itemRect.width());
+        case d3.layout.phrase.LEFT_TOP_TO_BOTTOM:
+        case d3.layout.phrase.LEFT_BOTTOM_TO_TOP:
+        case d3.layout.phrase.LEFT_LEFT_TO_RIGHT:
+        case d3.layout.phrase.LEFT_RIGHT_TO_LEFT:
+        case d3.layout.phrase.RIGHT_BOTTOM_TO_TOP:
+        case d3.layout.phrase.RIGHT_TOP_TO_BOTTOM:
+        case d3.layout.phrase.RIGHT_RIGHT_TO_LEFT:
+        case d3.layout.phrase.RIGHT_LEFT_TO_RIGHT:
+        case d3.layout.phrase.BOTTOM_LEFT_TO_RIGHT:
+        case d3.layout.phrase.BOTTOM_RIGHT_TO_LEFT:
+        case d3.layout.phrase.BOTTOM_BOTTOM_TO_TOP:
+        case d3.layout.phrase.BOTTOM_TOP_TO_BOTTOM:
+        case d3.layout.phrase.TOP_RIGHT_TO_LEFT:
+        case d3.layout.phrase.TOP_TOP_TO_BOTTOM:
+        case d3.layout.phrase.TOP_BOTTOM_TO_TOP:
+          return d3_treemap_rect(0, 0, 0, 0);
+        default:
+          throw("Invalid phrase");
+      }
+    }
+
+    // This function calculates the rects for each individual item in the chunk.
+    // It assumes that no items are added afterwards and that the algorithm has
+    // properly updated the chunk.rect for the items in this chunk.
+    chunk.itemRects = function() {
+      var rect = chunk.rect().clone(),
+          itemRect;
+
+      return chunk.items.map(function(item) {
+        itemRect = calculateItemRect(item, rect);
+        rect = reduce(rect, itemRect);
+        return itemRect;
+      });
+    }
+
     chunk.phrase = function() {
       return phrase;
     }
