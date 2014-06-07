7fb1a1e78b68fc22473d9f782795473468f310b7
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-06-07 14:13:32 +0200

    Make the grid conceptually a grid and add a test configuration.
    The test configuration gives more freedom in the allowed configuration
    settings to let one more freely play with the various settings. The other
    configurations are meant to represent a "useful" point in the design space
    so here we allow for less freedom.

--- src/configurations.js
+++ src/configurations.js
@@ -5,10 +5,19 @@
    * stretched.
    */
   var configurations = {
-
-    grid: {
-      itemSize: function() {
-        return 1;
+    Test: {
+      itemSize: {
+        allowed: ['CONSTANT', 'DATA'],
+        make: function(size) {
+          switch(size) {
+            case 'CONSTANT':
+              return function() { return 1; }
+            case 'DATA':
+              return function(item) { return +item; }
+            default:
+              throw('Free does not allow ' + size + ' as the size function');
+          }
+        }
       },
 
       phrase: {
@@ -29,6 +38,114 @@
           'TOP_LEFT_TO_RIGHT',
           'TOP_TOP_TO_BOTTOM',
           'TOP_BOTTOM_TO_TOP'
+        ],
+
+        make: function(initialPhrase) {
+          if (this.allowed.indexOf(initialPhrase) === -1) {
+            throw('Grid does not allow ' + initialPhrase + ' as the initial phrase');
+          }
+          return function() {
+            return d3.layout.phrase[initialPhrase];
+          };
+        }
+      },
+
+      recurse: function() {
+        return false;
+      },
+
+      score: function(itemCount) { // Grid scoring function
+        var chunkItemCount = 0;
+        var itemCountWithIntSquare = Math.pow(Math.ceil(Math.sqrt(itemCount)), 2);
+        return function(chunk, item) {
+          var score = Math.pow(chunkItemCount, 2) < itemCountWithIntSquare ? 1 : 0;
+          if (score === 1) {
+            chunkItemCount++;
+          } else {
+            chunkItemCount = 0;
+          }
+          return score;
+        }
+      }
+    },
+
+    Grid: {
+      itemSize: {
+        allowed: ['CONSTANT'],
+        make: function(size) {
+          switch(size) {
+            case 'CONSTANT':
+              return function() { return 1; }
+            default:
+              throw('Grid does not allow ' + size + ' as the size function');
+          }
+        }
+      },
+
+      phrase: {
+        allowed: [
+          'LEFT_TOP_TO_BOTTOM',
+          'LEFT_BOTTOM_TO_TOP',
+          'BOTTOM_LEFT_TO_RIGHT',
+          'BOTTOM_RIGHT_TO_LEFT',
+          'RIGHT_BOTTOM_TO_TOP',
+          'RIGHT_TOP_TO_BOTTOM',
+          'TOP_RIGHT_TO_LEFT',
+          'TOP_LEFT_TO_RIGHT'
+        ],
+
+        make: function(initialPhrase) {
+          if (this.allowed.indexOf(initialPhrase) === -1) {
+            throw('Grid does not allow ' + initialPhrase + ' as the initial phrase');
+          }
+          return function() {
+            return d3.layout.phrase[initialPhrase];
+          };
+        }
+      },
+
+      recurse: function() {
+        return false;
+      },
+
+      score: function(itemCount) { // Grid scoring function
+        var chunkItemCount = 0;
+        var itemCountWithIntSquare = Math.pow(Math.ceil(Math.sqrt(itemCount)), 2);
+        return function(chunk, item) {
+          var score = Math.pow(chunkItemCount, 2) < itemCountWithIntSquare ? 1 : 0;
+          if (score === 1) {
+            chunkItemCount++;
+          } else {
+            chunkItemCount = 0;
+          }
+          return score;
+        }
+      }
+    },
+
+    Grid: {
+      itemSize: {
+        allowed: ['CONSTANT'],
+        make: function(size) {
+          switch(size) {
+            case 'CONSTANT':
+              return function() { return 1; }
+            default:
+              throw('Grid does not allow ' + size + ' as the size function');
+          }
+        }
+      },
+
+      phrase: {
+        allowed: [
+          'LEFT_TOP_TO_BOTTOM',
+          'LEFT_BOTTOM_TO_TOP',
+          'BOTTOM_LEFT_TO_RIGHT',
+          'BOTTOM_RIGHT_TO_LEFT',
+          'RIGHT_BOTTOM_TO_TOP',
+          'RIGHT_TOP_TO_BOTTOM',
+          'TOP_RIGHT_TO_LEFT',
+          'TOP_LEFT_TO_RIGHT'
         ],
 
         make: function(initialPhrase) {
